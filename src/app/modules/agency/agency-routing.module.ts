ac12fcfbb3298fc07d6f132a62e22e6f44bbbc4e
Shibinshad <shibinshadshibu@gmail.com>
2024-02-27 14:54:31 +0530

    form created for uploading image etc

--- src/app/modules/agency/agency-routing.module.ts
+++ src/app/modules/agency/agency-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AgencyHomeComponent } from './agency-home/agency-home.component';
 import { LocationFormComponent } from './location-form/location-form.component';
+import { CateringFormComponent } from './catering-form/catering-form.component';
+import { MediaFormComponent } from './media-form/media-form.component';
+import { DecorationFormComponent } from './decoration-form/decoration-form.component';
+
 
 const routes: Routes = [
   { path: 'home', component: AgencyHomeComponent },
   { path: 'locationForm', component: LocationFormComponent },
+  { path:'cateringForm',component:CateringFormComponent},
+  { path:'mediaForm',component:MediaFormComponent},
+  { path:'decorationForm',component:DecorationFormComponent},
+
+
 ];
 
 @NgModule({
--- src/app/modules/agency/agency.module.ts
+++ src/app/modules/agency/agency.module.ts
@@ -4,9 +4,13 @@ import { AgencyComponent } from './agency.component';
 import { AgencyHomeComponent } from './agency-home/agency-home.component';
 import { AgencyRoutingModule } from './agency-routing.module';
 import { LocationFormComponent } from './location-form/location-form.component';
+import { CateringFormComponent } from './catering-form/catering-form.component';
+import { FormsModule } from '@angular/forms';
+import { MediaFormComponent } from './media-form/media-form.component';
+import { DecorationFormComponent } from './decoration-form/decoration-form.component';
 
 @NgModule({
-  declarations: [AgencyComponent, AgencyHomeComponent, LocationFormComponent],
-  imports: [CommonModule, AgencyRoutingModule],
+  declarations: [AgencyComponent, AgencyHomeComponent, LocationFormComponent, CateringFormComponent, MediaFormComponent, DecorationFormComponent],
+  imports: [CommonModule, AgencyRoutingModule,FormsModule],
 })
 export class AgencyModule {}
